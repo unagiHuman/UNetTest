f5ea4343bae0f2dc3fda3bec48b23ce23fc4048d
isaoohta <tigerwoods1206@yahoo.co.jp>
2016-04-19 00:19:30 +0900

    Init

---
+++ Scripts/ClientManager.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System;
+
+public class ClientManager : NetworkBehaviour {
+
+	public static ClientManager singleton {
+		get; set;
+	}
+
+	[SerializeField] GameObject viewPrefab;
+
+	Action	m_task;
+
+	Action m_callback;
+
+	View m_view;
+
+	[ServerCallback]
+	void Start () {
+	
+	}
+
+	public override void OnStartLocalPlayer ()
+	{
+		base.OnStartLocalPlayer ();
+		singleton = this;
+		var obj = Instantiate(viewPrefab) as GameObject;
+		m_view = obj.GetComponent<View>();
+		m_view.Init();
+	}
+	
+	[ServerCallback]
+	void Update () {
+		if(m_task!=null){
+			m_task();
+		}
+	}
+		
+	public void Request(string msg, Action callback){
+		m_callback = callback;
+		CmdRequest(msg);
+	}
+
+	[Command]
+	void CmdRequest(string msg){
+		CreateData(msg);
+	}
+
+	[Server]
+	void CreateData(string msg){
+		RpcGetData(msg);
+	}
+
+	[ClientRpc]
+	void RpcGetData(string msg){
+		Debug.Log(msg);
+		m_callback();
+	}
+
+
+	public void Request2(string msg, Action callback){
+		m_callback = callback;
+		CmdRequest2(msg);
+	}
+
+
+	[Command]
+	void CmdRequest2(string msg){
+		CreateData2(msg);
+	}
+
+	[Server]
+	void CreateData2(string msg){
+
+		RpcGetData2(msg);
+	}
+
+	[ClientRpc]
+	void RpcGetData2(string msg){
+		Debug.Log(msg);
+		m_callback();
+	}
+
+
+}
---
+++ Scripts/View.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class View : MonoBehaviour {
+
+	Action m_callback;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Init(){
+		ClientManager.singleton.Request("msg",CallBack);
+	}
+
+	void CallBack(){
+		ClientManager.singleton.Request2("msg2",EndCallBack);
+	}
+
+	void EndCallBack(){
+		Debug.Log("end");
+	}
+
+}
